9a7d8b6cf54f226094a5a3fb8959569084a65c34
Xenion1987 <39803750+Xenion1987@users.noreply.github.com>
2022-06-29 23:28:00 +0200

    update readme and namings

--- prepare-init-scripts.sh
+++ prepare-init-scripts.sh
@@ -8,7 +8,7 @@ mkdir -p ./mariadb/init
 if [ ${#db1[@]} -ge 5 ]; then
     while read -r db_name db_tables db_username db_host db_password db_privileges; do
         sed s/REPLACE_DATABASE_NAME/"${db_name}"/g ./mariadb/template.sql >./mariadb/init/1.sql
-        sed -i s/REPLACE_DATABASE_TABLE/"${db_tables}"/g ./mariadb/init/1.sql
+        sed -i s/REPLACE_DATABASE_TABLES/"${db_tables}"/g ./mariadb/init/1.sql
         sed -i s/REPLACE_USERNAME/"${db_username}"/g ./mariadb/init/1.sql
         sed -i s/REPLACE_HOSTNAME/"${db_host}"/g ./mariadb/init/1.sql
         sed -i s/REPLACE_PASSWORD/"${db_password}"/g ./mariadb/init/1.sql
@@ -19,7 +19,7 @@ fi
 # if [ ${#db2[@]} -ge 5 ]; then
 #     while read -r db_name db_tables db_username db_host db_password db_privileges; do
 #         sed s/REPLACE_DATABASE_NAME/"${db_name}"/g ./mariadb/template.sql >./mariadb/init/2.sql
-#         sed -i s/REPLACE_DATABASE_TABLE/"${db_tables}"/g ./mariadb/init/2.sql
+#         sed -i s/REPLACE_DATABASE_TABLES/"${db_tables}"/g ./mariadb/init/2.sql
 #         sed -i s/REPLACE_USERNAME/"${db_username}"/g ./mariadb/init/2.sql
 #         sed -i s/REPLACE_HOSTNAME/"${db_host}"/g ./mariadb/init/2.sql
 #         sed -i s/REPLACE_PASSWORD/"${db_password}"/g ./mariadb/init/2.sql
@@ -30,7 +30,7 @@ fi
 # if [ ${#db3[@]} -ge 5 ]; then
 #     while read -r db_name db_tables db_username db_host db_password db_privileges; do
 #         sed s/REPLACE_DATABASE_NAME/"${db_name}"/g ./mariadb/template.sql >./mariadb/init/3.sql
-#         sed -i s/REPLACE_DATABASE_TABLE/"${db_tables}"/g ./mariadb/init/3.sql
+#         sed -i s/REPLACE_DATABASE_TABLES/"${db_tables}"/g ./mariadb/init/3.sql
 #         sed -i s/REPLACE_USERNAME/"${db_username}"/g ./mariadb/init/3.sql
 #         sed -i s/REPLACE_HOSTNAME/"${db_host}"/g ./mariadb/init/3.sql
 #         sed -i s/REPLACE_PASSWORD/"${db_password}"/g ./mariadb/init/3.sql
@@ -41,7 +41,7 @@ fi
 # if [ ${#db4[@]} -ge 5 ]; then
 #     while read -r db_name db_tables db_username db_host db_password db_privileges; do
 #         sed s/REPLACE_DATABASE_NAME/"${db_name}"/g ./mariadb/template.sql >./mariadb/init/4.sql
-#         sed -i s/REPLACE_DATABASE_TABLE/"${db_tables}"/g ./mariadb/init/4.sql
+#         sed -i s/REPLACE_DATABASE_TABLES/"${db_tables}"/g ./mariadb/init/4.sql
 #         sed -i s/REPLACE_USERNAME/"${db_username}"/g ./mariadb/init/4.sql
 #         sed -i s/REPLACE_HOSTNAME/"${db_host}"/g ./mariadb/init/4.sql
 #         sed -i s/REPLACE_PASSWORD/"${db_password}"/g ./mariadb/init/4.sql
@@ -52,7 +52,7 @@ fi
 # if [ ${#db5[@]} -ge 5 ]; then
 #     while read -r db_name db_tables db_username db_host db_password db_privileges; do
 #         sed s/REPLACE_DATABASE_NAME/"${db_name}"/g ./mariadb/template.sql >./mariadb/init/5.sql
-#         sed -i s/REPLACE_DATABASE_TABLE/"${db_tables}"/g ./mariadb/init/5.sql
+#         sed -i s/REPLACE_DATABASE_TABLES/"${db_tables}"/g ./mariadb/init/5.sql
 #         sed -i s/REPLACE_USERNAME/"${db_username}"/g ./mariadb/init/5.sql
 #         sed -i s/REPLACE_HOSTNAME/"${db_host}"/g ./mariadb/init/5.sql
 #         sed -i s/REPLACE_PASSWORD/"${db_password}"/g ./mariadb/init/5.sql
